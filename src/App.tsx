b5acddc36cec18bd49779d3e920fea179c5dea6e
alfarotama <alfarohadiyantama13@gmail.com>
2021-08-17 18:42:11 +0700

    feat:Adding landing page & configure redux on index ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import {
+	Box,
+	Flex,
+	Grid,
+	Heading,
+	Text,
+	Center,
+	Image,
+	Button,
+} from "@chakra-ui/react";
+// import { FaSpotify } from 'react-icons/fa'
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<Box maxW="100vw" p="2rem">
+			<Flex justify={"space-between"} align={"center"}>
+				<Text fontSize={"xl"} fontWeight={900}>
+					Ur Music Player
+				</Text>
+				<Button>Login with Spotify</Button>
+			</Flex>
+			<Grid templateColumns="1fr 1fr" gap="5vw" minH="100vh">
+				<Flex direction={"column"} justify={"center"}>
+					<Heading as={"h1"} fontSize={"6xl"}>
+						Temukan Musik Mu Dan Nikmati Dalam Ketenangan.
+					</Heading>
+					<Text my={4} color={"gray.400"}>
+						Ur Music Player Adalah Tugas Akhir Dari Seorang Yang Mencintai Dunia
+						Coding Dan Belajar Melalui Jaringan Teman Dan Otodidak
+					</Text>
+					<Box>
+						<Button size={"lg"}>Login with Spotify</Button>
+					</Box>
+				</Flex>
+				<Center>
+					<Image src={"https://picsum.photos/500"} />
+				</Center>
+			</Grid>
+		</Box>
+	);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ChakraProvider } from "@chakra-ui/react";
-import { Provider as ReduxProvider } from "react-redux";
+import App from "./App";
 import theme from "./theme";
 import store from "./store";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider as ReduxProvider } from "react-redux";
 
 ReactDOM.render(
 	<ReduxProvider store={store}>
-		<ChakraProvider theme={theme}>
+		<ChakraProvider resetCSS theme={theme}>
 			<App />
 		</ChakraProvider>
 	</ReduxProvider>,
